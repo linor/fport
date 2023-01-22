b06462eb5bfb5b55fd82bd284b2d1b632adb0b64
Krijn Schaap <krijnschaap@gmail.com>
2023-01-22 22:13:38 +0100

    Initial commit.

---
+++ src/fport.cpp
@@ -0,0 +1,342 @@
+#include <Arduino.h>
+#include "fport.h"
+#include "frsky_crc.h"
+
+#define FPORT_TIME_NEEDED_PER_FRAME_US 3000
+#define FPORT_MAX_TELEMETRY_RESPONSE_DELAY_US 2000
+#define FPORT_MIN_TELEMETRY_RESPONSE_DELAY_US 500
+#define FPORT_MAX_TELEMETRY_AGE_MS 500
+
+#define FPORT_TELEMETRY_MAX_CONSECUTIVE_TELEMETRY_FRAMES 2
+
+
+#define FPORT_FRAME_MARKER 0x7E
+
+#define FPORT_ESCAPE_CHAR 0x7D
+#define FPORT_ESCAPE_MASK 0x20
+
+#define FPORT_BAUDRATE 115200
+
+enum
+{
+    FSSP_START_STOP = 0x7E,
+
+    FSSP_DLE        = 0x7D,
+    FSSP_DLE_XOR    = 0x20,
+
+    FSSP_DATA_FRAME = 0x10,
+};
+
+enum {
+    FPORT_FRAME_TYPE_CONTROL = 0x00,
+    FPORT_FRAME_TYPE_TELEMETRY_REQUEST = 0x01,
+    FPORT_FRAME_TYPE_TELEMETRY_RESPONSE = 0x81,
+
+};
+
+enum {
+    FPORT_FRAME_ID_NULL = 0x00,     // (master/slave)
+    FPORT_FRAME_ID_DATA = 0x10,     // (master/slave)
+    FPORT_FRAME_ID_READ = 0x30,     // (master)
+    FPORT_FRAME_ID_WRITE = 0x31,    // (master)
+    FPORT_FRAME_ID_RESPONSE = 0x32, // (slave)
+};
+
+#define SBUS_FLAG_CHANNEL_17        (1 << 0)
+#define SBUS_FLAG_CHANNEL_18        (1 << 1)
+
+#define SBUS_DIGITAL_CHANNEL_MIN 173
+#define SBUS_DIGITAL_CHANNEL_MAX 1812
+
+#define SBUS_FLAG_SIGNAL_LOSS       (1 << 2)
+#define SBUS_FLAG_FAILSAFE_ACTIVE   (1 << 3)
+
+void FPort::begin(HardwareSerial *port) {
+    this->_port = port;
+
+    _port->begin(FPORT_BAUDRATE);
+}
+
+void FPort::receiveData(uint8_t val) {
+    const unsigned long currentTimeUs = micros(); 
+
+    _clearToSend = false;
+
+    if (_framePosition > 1 && (currentTimeUs - _frameStartAt) > FPORT_TIME_NEEDED_PER_FRAME_US + 500) {
+        // TODO: reportFrameError(DEBUG_FPORT_ERROR_TIMEOUT);
+        Serial.println("Timeout!");
+        _framePosition = 0;
+    }
+
+    if (val == FPORT_FRAME_MARKER) {
+        if (_framePosition > 1) {
+            const uint8_t nextWriteIndex = (_rxBufferWriteIndex + 1) % rxBuffers;
+            if (nextWriteIndex != _rxBufferReadIndex) {
+                _rxBuffer[_rxBufferWriteIndex].length = _framePosition - 1;
+                _rxBufferWriteIndex = nextWriteIndex;
+            }
+
+            if (_telemetryFrame) {
+                _clearToSend = true;
+                _lastTelemetryFrameReceivedUs = currentTimeUs;
+                _telemetryFrame = false;
+            }
+
+            _lastFrameReceivedUs = currentTimeUs;
+            _escapedCharacter = false;
+        }
+
+        _frameStartAt = currentTimeUs;
+        _framePosition = 1;
+
+        _rxBuffer[_rxBufferWriteIndex].frameStartTimeUs = currentTimeUs;
+    } else if (_framePosition > 0) {
+        if (_framePosition >= FPORT_BUFFER_SIZE + 1) {
+            _framePosition = 0;
+
+            // TODO:    reportFrameError(DEBUG_FPORT_ERROR_OVERSIZE);
+            Serial.println("Error oversize!");
+        } else {
+            if (_escapedCharacter) {
+                val = val ^ FPORT_ESCAPE_MASK;
+                _escapedCharacter = false;
+            } else if (val == FPORT_ESCAPE_CHAR) {
+                _escapedCharacter = true;
+                return;
+            }
+
+            if (_framePosition == 2 && val == FPORT_FRAME_TYPE_TELEMETRY_REQUEST) {
+                _telemetryFrame = true;
+            }
+
+            _rxBuffer[_rxBufferWriteIndex].data[_framePosition - 1] = val;
+            _framePosition = _framePosition + 1;
+        }
+    }
+}
+
+uint8_t FPort::decodeSBusChannels(const sbusChannels_t *sbusChannels) {
+    channels[0] = sbusChannels->chan0;
+    channels[1] = sbusChannels->chan1;
+    channels[2] = sbusChannels->chan2;
+    channels[3] = sbusChannels->chan3;
+    channels[4] = sbusChannels->chan4;
+    channels[5] = sbusChannels->chan5;
+    channels[6] = sbusChannels->chan6;
+    channels[7] = sbusChannels->chan7;
+    channels[8] = sbusChannels->chan8;
+    channels[9] = sbusChannels->chan9;
+    channels[10] = sbusChannels->chan10;
+    channels[11] = sbusChannels->chan11;
+    channels[12] = sbusChannels->chan12;
+    channels[13] = sbusChannels->chan13;
+    channels[14] = sbusChannels->chan14;
+    channels[15] = sbusChannels->chan15;
+
+    if (sbusChannels->flags & SBUS_FLAG_CHANNEL_17) {
+        channels[16] = SBUS_DIGITAL_CHANNEL_MAX;
+    } else {
+        channels[16] = SBUS_DIGITAL_CHANNEL_MIN;
+    }
+
+    if (sbusChannels->flags & SBUS_FLAG_CHANNEL_18) {
+        channels[17] = SBUS_DIGITAL_CHANNEL_MAX;
+    } else {
+        channels[17] = SBUS_DIGITAL_CHANNEL_MIN;
+    }
+
+    if (sbusChannels->flags & SBUS_FLAG_FAILSAFE_ACTIVE) {
+        // internal failsafe enabled and rx failsafe flag set
+        // RX *should* still be sending valid channel data (repeated), so use it.
+        return RX_FRAME_COMPLETE | RX_FRAME_FAILSAFE;
+    }
+
+    if (sbusChannels->flags & SBUS_FLAG_SIGNAL_LOSS) {
+        // The received data is a repeat of the last valid data so can be considered complete.
+        return RX_FRAME_COMPLETE | RX_FRAME_DROPPED;
+    }
+
+    return RX_FRAME_COMPLETE;
+}
+
+uint8_t FPort::parsePendingData() {
+    uint8_t result = RX_FRAME_PENDING;
+
+    if (_rxBufferReadIndex != _rxBufferWriteIndex) {
+        uint8_t bufferLength = _rxBuffer[_rxBufferReadIndex].length;
+        uint8_t frameLength = _rxBuffer[_rxBufferReadIndex].data[0];
+        if (frameLength != bufferLength - 2) {
+            // reportFrameError(DEBUG_FPORT_ERROR_SIZE);
+            // TODO
+            Serial.println("Fport frame length error");
+        } else {
+            if (!frskyCheckSumIsGood(&_rxBuffer[_rxBufferReadIndex].data[0], bufferLength)) {
+                // TODO: reportFrameError(DEBUG_FPORT_ERROR_CHECKSUM);
+                Serial.println("checksum error");
+            } else {
+                fportFrame_t *frame = (fportFrame_t *)&_rxBuffer[_rxBufferReadIndex].data[1];
+
+                switch (frame->type) {
+                    case FPORT_FRAME_TYPE_CONTROL:
+                        if (frameLength != FPORT_FRAME_PAYLOAD_LENGTH_CONTROL) {
+                            // TODO reportFrameError(DEBUG_FPORT_ERROR_TYPE_SIZE);
+                            Serial.println("Error Type Size");
+                        } else {
+                            result = decodeSBusChannels(&frame->data.controlData.channels);
+                            _lastRcFrameReceivedMs = millis();
+
+                            if (!(result & (RX_FRAME_FAILSAFE | RX_FRAME_DROPPED))) {
+                                _lastRcFrameTimeUs = _rxBuffer[_rxBufferReadIndex].frameStartTimeUs;
+                            }
+                        }
+                        break;
+                    case FPORT_FRAME_TYPE_TELEMETRY_REQUEST:
+                        if (frameLength != FPORT_FRAME_PAYLOAD_LENGTH_TELEMETRY_REQUEST) {
+                            // TODO reportFrameError(DEBUG_FPORT_ERROR_TYPE_SIZE);
+                            Serial.println("Error size");
+                        } else {
+                            switch(frame->data.telemetryData.frameId) {
+                                case FPORT_FRAME_ID_DATA:
+                                    if (!_rxDrivenFrameRate) {
+                                        _rxDrivenFrameRate = true;
+                                    }
+
+                                    _hasTelemetryRequest = true;
+                                    break;
+                                case FPORT_FRAME_ID_NULL:
+                                    if (!_rxDrivenFrameRate) {
+                                        if (_consecutiveTelemetryFrameCount >= FPORT_TELEMETRY_MAX_CONSECUTIVE_TELEMETRY_FRAMES) {
+                                            _consecutiveTelemetryFrameCount = 0;
+                                        } else {
+                                            _hasTelemetryRequest = true;
+                                            _consecutiveTelemetryFrameCount++;
+                                        }
+                                    }
+
+                                    break;
+                                case FPORT_FRAME_ID_READ:
+                                case FPORT_FRAME_ID_WRITE: // never used
+                                /*
+                                    memcpy(&payloadBuffer, &frame->data.telemetryData, sizeof(smartPortPayload_t));
+                                    mspPayload = &payloadBuffer;
+*/
+                                    break;
+                                default:
+                                    break;
+                            }
+                        }
+
+                        break;
+                    default:
+                        // TODO reportFrameError(DEBUG_FPORT_ERROR_TYPE);
+                        break;
+                }
+            }
+        }
+
+        _rxBufferReadIndex = (_rxBufferReadIndex + 1) % rxBuffers;
+    }
+
+    if (_hasTelemetryRequest && (micros() - _lastTelemetryFrameReceivedUs) >= FPORT_MIN_TELEMETRY_RESPONSE_DELAY_US) {
+        _hasTelemetryRequest = false;
+
+        result = (result & ~RX_FRAME_PENDING) | RX_FRAME_PROCESSING_REQUIRED;
+    }
+
+    if (_lastRcFrameReceivedMs && ((millis() - _lastRcFrameReceivedMs) > FPORT_MAX_TELEMETRY_AGE_MS)) {
+        _lastRcFrameReceivedMs = 0;
+    }
+
+    return result;
+}
+
+void FPort::addSensor(sensorType sensorId, sensorDataFnPtr data, unsigned long intervalMs) {
+    if (_sensorCount >= maxSensors) return;
+    _sensors[_sensorCount].type = sensorId;
+    _sensors[_sensorCount].interval = intervalMs;
+    _sensors[_sensorCount].fn = data;
+    _sensorCount++;
+}
+
+void smartPortSendByte(uint8_t c, uint16_t *checksum, HardwareSerial *port)
+{
+    // smart port escape sequence
+    if (c == FSSP_DLE || c == FSSP_START_STOP) {
+        port->write(FSSP_DLE);
+        port->write(c ^ FSSP_DLE_XOR);
+    } else {
+        port->write(c);
+    }
+
+    if (checksum != NULL) {
+        frskyCheckSumStep(checksum, c);
+    }
+}
+
+void smartPortWriteFrameFport(HardwareSerial *port, const smartPortPayload_t *payload)
+{
+    uint16_t checksum = 0;
+    smartPortSendByte(FPORT_RESPONSE_FRAME_LENGTH, &checksum, port);
+    smartPortSendByte(FPORT_FRAME_TYPE_TELEMETRY_RESPONSE, &checksum, port);
+
+    uint8_t *data = (uint8_t *)payload;
+    for (unsigned i = 0; i < sizeof(smartPortPayload_t); i++) {
+        smartPortSendByte(*data++, &checksum, port);
+    }
+    
+    frskyCheckSumFini(&checksum);
+    smartPortSendByte(checksum, NULL, port);
+}
+
+void FPort::processTelemetry() {
+    unsigned long currentTimeUs = micros();
+    if ((currentTimeUs - _lastTelemetryFrameReceivedUs) > FPORT_MAX_TELEMETRY_RESPONSE_DELAY_US) {
+        _clearToSend = false;
+    }
+
+    if (_clearToSend) {
+        unsigned long timeout = _lastTelemetryFrameReceivedUs + FPORT_MAX_TELEMETRY_RESPONSE_DELAY_US;
+        while(_clearToSend & ((timeout - micros()) > 0)) {
+            unsigned long currentTimeMs = millis();
+
+            for(int sensor = 0; sensor < _sensorCount; sensor++) {
+                if (currentTimeMs > _sensors[sensor].nextUpdate) {
+                    smartPortPayload_t payload;
+                    payload.frameId = FSSP_DATA_FRAME;
+                    payload.valueId = _sensors[sensor].type;
+                    payload.data = _sensors[sensor].fn(_sensors[sensor].type);
+
+                    smartPortWriteFrameFport(_port, &payload);
+                    _sensors[sensor].nextUpdate = currentTimeMs + _sensors[sensor].interval;
+                    _clearToSend = false;
+                    break;
+                }
+            }
+        }
+
+        if (_clearToSend) {
+//            smartPortWriteFrameFport(this->_port, &emptySmartPortFrame);
+            _clearToSend = false;
+        }
+
+        _lastTelemetryFrameSentUs = currentTimeUs;
+    }
+}
+
+uint8_t FPort::loop() {
+    if (_port->available()) {
+        char c = _port->read();
+        receiveData(c);
+    }
+
+    uint8_t status = parsePendingData();
+    if (status & RX_FRAME_PROCESSING_REQUIRED) {
+        processTelemetry();
+    }
+
+    if ((micros() - _lastFrameReceivedUs) > (2 * FPORT_TIME_NEEDED_PER_FRAME_US)) {
+        status |= RX_TIMEOUT;
+    }
+
+    return status;
+}
---
+++ src/fport.h
@@ -0,0 +1,190 @@
+#pragma once
+
+typedef enum
+{
+    FSSP_DATAID_SPEED      = 0x0830 ,
+    FSSP_DATAID_VFAS       = 0x0210 ,
+    FSSP_DATAID_VFAS1      = 0x0211 ,
+    FSSP_DATAID_VFAS2      = 0x0212 ,
+    FSSP_DATAID_VFAS3      = 0x0213 ,
+    FSSP_DATAID_VFAS4      = 0x0214 ,
+    FSSP_DATAID_VFAS5      = 0x0215 ,
+    FSSP_DATAID_VFAS6      = 0x0216 ,
+    FSSP_DATAID_VFAS7      = 0x0217 ,
+    FSSP_DATAID_VFAS8      = 0x0218 ,
+    FSSP_DATAID_CURRENT    = 0x0200 ,
+    FSSP_DATAID_CURRENT1   = 0x0201 ,
+    FSSP_DATAID_CURRENT2   = 0x0202 ,
+    FSSP_DATAID_CURRENT3   = 0x0203 ,
+    FSSP_DATAID_CURRENT4   = 0x0204 ,
+    FSSP_DATAID_CURRENT5   = 0x0205 ,
+    FSSP_DATAID_CURRENT6   = 0x0206 ,
+    FSSP_DATAID_CURRENT7   = 0x0207 ,
+    FSSP_DATAID_CURRENT8   = 0x0208 ,
+    FSSP_DATAID_RPM        = 0x0500 ,
+    FSSP_DATAID_RPM1       = 0x0501 ,
+    FSSP_DATAID_RPM2       = 0x0502 ,
+    FSSP_DATAID_RPM3       = 0x0503 ,
+    FSSP_DATAID_RPM4       = 0x0504 ,
+    FSSP_DATAID_RPM5       = 0x0505 ,
+    FSSP_DATAID_RPM6       = 0x0506 ,
+    FSSP_DATAID_RPM7       = 0x0507 ,
+    FSSP_DATAID_RPM8       = 0x0508 ,
+    FSSP_DATAID_ALTITUDE   = 0x0100 ,
+    FSSP_DATAID_FUEL       = 0x0600 ,
+    FSSP_DATAID_ADC1       = 0xF102 ,
+    FSSP_DATAID_ADC2       = 0xF103 ,
+    FSSP_DATAID_LATLONG    = 0x0800 ,
+    FSSP_DATAID_VARIO      = 0x0110 ,
+    FSSP_DATAID_CELLS      = 0x0300 ,
+    FSSP_DATAID_CELLS_LAST = 0x030F ,
+    FSSP_DATAID_HEADING    = 0x0840 ,
+// DIY range 0x5100 to 0x52FF
+    FSSP_DATAID_CAP_USED   = 0x5250 ,
+#if defined(USE_ACC)
+    FSSP_DATAID_PITCH      = 0x5230 , // custom
+    FSSP_DATAID_ROLL       = 0x5240 , // custom
+    FSSP_DATAID_ACCX       = 0x0700 ,
+    FSSP_DATAID_ACCY       = 0x0710 ,
+    FSSP_DATAID_ACCZ       = 0x0720 ,
+#endif
+    FSSP_DATAID_T1         = 0x0400 ,
+    FSSP_DATAID_T11        = 0x0401 ,
+    FSSP_DATAID_T2         = 0x0410 ,
+    FSSP_DATAID_HOME_DIST  = 0x0420 ,
+    FSSP_DATAID_GPS_ALT    = 0x0820 ,
+    FSSP_DATAID_ASPD       = 0x0A00 ,
+    FSSP_DATAID_TEMP       = 0x0B70 ,
+    FSSP_DATAID_TEMP1      = 0x0B71 ,
+    FSSP_DATAID_TEMP2      = 0x0B72 ,
+    FSSP_DATAID_TEMP3      = 0x0B73 ,
+    FSSP_DATAID_TEMP4      = 0x0B74 ,
+    FSSP_DATAID_TEMP5      = 0x0B75 ,
+    FSSP_DATAID_TEMP6      = 0x0B76 ,
+    FSSP_DATAID_TEMP7      = 0x0B77 ,
+    FSSP_DATAID_TEMP8      = 0x0B78 ,
+    FSSP_DATAID_A3         = 0x0900 ,
+    FSSP_DATAID_A4         = 0x0910
+} sensorType;
+
+typedef enum {
+    RX_FRAME_PENDING = 0,
+    RX_FRAME_COMPLETE = (1 << 0),
+    RX_FRAME_FAILSAFE = (1 << 1),
+    RX_FRAME_PROCESSING_REQUIRED = (1 << 2),
+    RX_FRAME_DROPPED = (1 << 3),
+    RX_TIMEOUT = (1 << 4)
+} frameState;
+
+typedef struct  {
+    // 176 bits of data (11 bits per channel * 16 channels) = 22 bytes.
+    unsigned int chan0 : 11;
+    unsigned int chan1 : 11;
+    unsigned int chan2 : 11;
+    unsigned int chan3 : 11;
+    unsigned int chan4 : 11;
+    unsigned int chan5 : 11;
+    unsigned int chan6 : 11;
+    unsigned int chan7 : 11;
+    unsigned int chan8 : 11;
+    unsigned int chan9 : 11;
+    unsigned int chan10 : 11;
+    unsigned int chan11 : 11;
+    unsigned int chan12 : 11;
+    unsigned int chan13 : 11;
+    unsigned int chan14 : 11;
+    unsigned int chan15 : 11;
+    uint8_t flags;
+} __attribute__((__packed__)) sbusChannels_t;
+
+typedef struct {
+    uint8_t  frameId;
+    uint16_t valueId;
+    uint32_t data;
+} __attribute__((packed)) smartPortPayload_t;
+
+typedef struct fportControlData_s {
+    sbusChannels_t channels;
+    uint8_t rssi;
+} fportControlData_t;
+
+typedef union fportData_s {
+    fportControlData_t controlData;
+    smartPortPayload_t telemetryData;
+} fportData_t;
+
+typedef struct fportFrame_s {
+    uint8_t type;
+    fportData_t data;
+} fportFrame_t;
+
+typedef uint32_t (*sensorDataFnPtr)(const sensorType sensorId);
+
+typedef struct {
+    sensorType type;
+    sensorDataFnPtr fn;
+    unsigned long interval;
+    unsigned long nextUpdate;
+} sensorData_t;
+
+static const smartPortPayload_t emptySmartPortFrame = { .frameId = 0, .valueId = 0, .data = 0 };
+
+#define FPORT_REQUEST_FRAME_LENGTH sizeof(fportFrame_t)
+#define FPORT_RESPONSE_FRAME_LENGTH (sizeof(uint8_t) + sizeof(smartPortPayload_t))
+
+#define FPORT_FRAME_PAYLOAD_LENGTH_CONTROL (sizeof(uint8_t) + sizeof(fportControlData_t))
+#define FPORT_FRAME_PAYLOAD_LENGTH_TELEMETRY_REQUEST (sizeof(uint8_t) + sizeof(smartPortPayload_t))
+
+#define FPORT_BUFFER_SIZE (FPORT_REQUEST_FRAME_LENGTH + 2 * sizeof(uint8_t))
+
+class HardwareSerial;
+
+class FPort {
+    public:
+        void begin(HardwareSerial *port);
+        void addSensor(sensorType sensorId, sensorDataFnPtr data, unsigned long intervalMs = 1000);
+        uint8_t loop();
+
+        uint16_t channels[18];
+    private:
+        static const int rxBuffers = 3;
+        static const int maxSensors = 20;
+
+        typedef struct {
+            uint8_t data[FPORT_BUFFER_SIZE];
+            uint8_t length;
+            unsigned long frameStartTimeUs;
+        } buffer;
+
+        HardwareSerial *_port;
+
+        sensorData_t _sensors[maxSensors];
+        uint8_t _sensorCount = 0;
+        uint8_t _currentSensor;
+
+        unsigned long _frameStartAt = 0;
+        unsigned long _lastFrameReceivedUs = 0;
+        unsigned long _lastTelemetryFrameReceivedUs = 0;
+        unsigned long _lastTelemetryFrameSentUs = 0;
+        unsigned long _lastRcFrameReceivedMs = 0;
+        unsigned long _lastRcFrameTimeUs = 0;
+
+        bool _clearToSend = false;
+        bool _telemetryFrame = false;
+
+        buffer _rxBuffer[rxBuffers];
+        uint8_t _rxBufferWriteIndex = 0;
+        uint8_t _rxBufferReadIndex = 0;
+        uint8_t _framePosition = 0;
+        bool _escapedCharacter = false;
+
+        bool _hasTelemetryRequest = false;
+        bool _rxDrivenFrameRate = false;
+        uint8_t _consecutiveTelemetryFrameCount = 0;   
+
+        void receiveData(uint8_t c);
+        uint8_t parsePendingData();
+        uint8_t decodeSBusChannels(const sbusChannels_t *channels);
+        void processTelemetry();
+};
+
